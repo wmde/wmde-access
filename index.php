3d4e9ac29d6bdc9298bf8b8e2e989de1126a4c27
addshore <addshore@users.noreply.github.com>
2019-02-19 15:24:28 +0000

    Remove 2 groups that noone from WMDE is now in

--- index.php
+++ index.php
@@ -25,9 +25,7 @@ const MG_PHABRICATOR = 'phabricator';
 
 $groupsToCheck = [
 	MG_LDAP_PUPPET => [
-		'restricted',
 		'deployment',
-		'mw-log-readers',
 		'researchers',
 		'analytics-privatedata-users',
 		'analytics-wmde-users',
