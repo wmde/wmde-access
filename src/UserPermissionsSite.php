655698c3da20afc5cb511bbe28ca1586290a051b
Leszek Manicki <leszek.manicki@wikimedia.de>
2022-01-28 20:21:48 +0100

    Added column definitions to UserPermissionsSite
    For now hardcoded

--- src/UserPermissionsSite.php
+++ src/UserPermissionsSite.php
@@ -34,8 +34,72 @@ class UserPermissionsSite {
 			$userData[$user] = [];
 		}
 
+		$columns = [
+			'ldap-magic' => [
+				'category' => 'LDAP magic',
+				'columnCount' => 2,
+				'columns' => [
+					'ldap-wmde' => [ 'label' => 'wmde' ],
+					'ldap-nda' => [ 'label' => 'nda' ],
+				],
+			],
+			'ldap-puppet' => [
+				'category' => 'LDAP operations-puppet',
+				'columnCount' => 8,
+				'columns' => [
+					[ 'label' => 'deployment' ],
+					[ 'label' => 'wdqs-admins' ],
+					[ 'label' => 'analytics-privatedata-users' ],
+					[ 'label' => 'analytics-wmde-users' ],
+					[ 'label' => 'contint-admins' ],
+					[ 'label' => 'contint-docker' ],
+					[ 'label' => 'releasers-wikibase' ],
+					[ 'label' => 'releasers-wikidiff2' ],
+				],
+			],
+			'ldap-cloud-projects' => [
+				'category' => 'Cloud VPS',
+				'columnCount' => 5,
+				'columns' => [
+					[ 'label' => 'deployment-prep' ],
+					[ 'label' => 'lizenzhinweisgenerator' ],
+					[ 'label' => 'wikidata-dev' ],
+					[ 'label' => 'wikidata-query' ],
+					[ 'label' => 'wmde-dashboards' ],
+				],
+			],
+			'gerrit' => [
+				'category' => 'Gerrit',
+				'columnCount' => 1,
+				'columns' => [
+					[
+						'label' => 'Gerrit Managers',
+						'url' => 'https://gerrit.wikimedia.org/r/#/admin/groups/119,members',
+					],
+				],
+			],
+			'phabricator' => [
+				'category' => 'Phabricator',
+				'columnCount' => 3,
+				'columns' => [
+					[
+						'label' => 'Project-Admins',
+						'url' => 'https://phabricator.wikimedia.org/project/members/1776/',
+					],
+					[
+						'label' => 'NDA',
+						'url' => 'https://phabricator.wikimedia.org/project/members/61/',
+					],
+					[
+						'label' => 'Security',
+						'url' => 'https://phabricator.wikimedia.org/project/members/30/',
+					],
+				],
+			],
+		];
+
 		return $this->template->render( [
-			'columnMetadata' => [],
+			'columnMetadata' => $columns,
 			'userData' => $userData,
 		] );
 	}
