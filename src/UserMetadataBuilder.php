c93e3a1053c4151728532eefd0ddfe0a113ccc0e
Leszek Manicki <leszek.manicki@wikimedia.de>
2023-09-29 20:37:27 +0200

    Added service that build User metadata objects from yaml config

---
+++ src/UserMetadataBuilder.php
@@ -0,0 +1,30 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace WMDE\PermissionsOverview;
+
+/**
+ * @licence BSD-3-Clause
+ */
+class UserMetadataBuilder {
+
+	private const KEY_WMF_LDAP = 'wmf-ldap';
+
+	private const KEY_WMF_PHABRICATOR = 'wmf-phabricator';
+
+	public function getUserMetadata( array $usersMetadata ): array {
+		$users = [];
+
+		foreach ( $usersMetadata as $canonicalName => $metadata ) {
+			$users[$canonicalName] = new User(
+				$canonicalName,
+				$metadata[self::KEY_WMF_LDAP] ?? $canonicalName, // ?? '',
+				$metadata[self::KEY_WMF_PHABRICATOR] ?? $canonicalName // ?? ''
+			);
+		}
+
+		return $users;
+	}
+
+}
